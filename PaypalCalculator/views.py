3c8e052ca25fcdce1d2ac7efbeddc2309399976a
Nyzrak <seloe@me.com>
2025-11-16 19:37:00 +0100

    adjustments

--- PaypalCalculator/views.py
+++ PaypalCalculator/views.py
@@ -3,28 +3,49 @@ from django.shortcuts import render
 
 # Create your views here.
 PRICE_ADDON_CONSTANTS = {
-        # no fees
-        "friends_family": {"rate_percent": 0, "flat_fee": 0.0},
-        # fees for private sellers
-        "goods_services": {"rate_percent": 2.49, "flat_fee": 0.35},
-        # fees for donations
-        "donations": {"rate_percent": 1.00, "flat_fee": 0.35},
-        # fees for micro payments (?)
-        "micro_payment": {"rate_percent": 3.25, "flat_fee": 0.10},
-        # fees for sellers/dealers between 0 - 2.000€
-        "dealer_condition_1": {"rate_percent": 0.50, "flat_fee": 0.35},
-        # fees for sellers/dealers between 2.000,01 - 5.000€
-        "dealer_condition_2": {"rate_percent": 4.75, "flat_fee": 0.35},
-        # fees for sellers/dealers between 5.000,01 - 25.000€
-        "dealer_condition_3": {"rate_percent": 2.00, "flat_fee": 0.35},
-        # fees for sellers/dealers between 25.000,01 - 100.000€
-        "dealer_condition_4": {"rate_percent": 1.50, "flat_fee": 0.35},
-        # fees for sellers/dealers above 100.000€
-        "dealer_condition_5": {"rate_percent": 1.49, "flat_fee": 0.35},
-    }
+    # no fees
+    "friends_family": {"rate_percent": 0, "flat_fee": 0.0},
+    # fees for private sellers
+    "goods_services": {"rate_percent": 2.49, "flat_fee": 0.35},
+    # fees for donations
+    "donations": {"rate_percent": 1.00, "flat_fee": 0.35},
+    # fees for micro payments (?)
+    "micro_payment": {"rate_percent": 3.25, "flat_fee": 0.10},
+    # fees for sellers/dealers between 0 - 2.000€
+    "dealer_condition_1": {"rate_percent": 0.50, "flat_fee": 0.35},
+    # fees for sellers/dealers between 2.000,01 - 5.000€
+    "dealer_condition_2": {"rate_percent": 4.75, "flat_fee": 0.35},
+    # fees for sellers/dealers between 5.000,01 - 25.000€
+    "dealer_condition_3": {"rate_percent": 2.00, "flat_fee": 0.35},
+    # fees for sellers/dealers between 25.000,01 - 100.000€
+    "dealer_condition_4": {"rate_percent": 1.50, "flat_fee": 0.35},
+    # fees for sellers/dealers above 100.000€
+    "dealer_condition_5": {"rate_percent": 1.49, "flat_fee": 0.35},
+}
+
+goods_type_labels = {
+    "friends_family": "Friends & Family",
+    "goods_services": "Goods & Services",
+    "donations": "Donations",
+    "micro_payment": "Micro Payment",
+    "dealer_condition_1": "Dealer Condition (between 0€ - 2.000€)",
+    "dealer_condition_2": "Dealer Condition (between 2.000,01€ - 5.000€)",
+    "dealer_condition_3": "Dealer Condition (between 5.000,01€ - 25.000€)",
+    "dealer_condition_4": "Dealer Condition (between 25.000,01€ - 100.000€)",
+    "dealer_condition_5": "Dealer Condition (above 100.000€)",
+}
 def calculate_fees(request):
     if request.method == 'GET':
-        return render(request, 'PaypalCalculator/calculate.html', {'goods_type': PRICE_ADDON_CONSTANTS})
+        goods_type_items = [
+            (key, goods_type_labels[key])
+            for key in PRICE_ADDON_CONSTANTS.keys()
+        ]
+        context = {
+            'goods_type_items': goods_type_items,
+            'goods_type': PRICE_ADDON_CONSTANTS,
+        }
+        return render(request, 'PaypalCalculator/calculate.html', context)
+
     result = None
     if request.method == 'POST':
         goods_type = request.POST.get('goods_type', '')
